026e5bedc62f8c906899b27e021c379c6371d04c
Ostap Yonka <ynk.otp.nl@gmail.com>
2026-05-28 16:12:05 +0300

    fix problems

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://ynkotp228.github.io",
+  base: "/cyberHub/",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
-
-  site: "https://ynkotp228.github.io",
-  base: "/cyberHub/",
 });
+ 
