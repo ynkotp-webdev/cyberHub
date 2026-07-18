ecf9015040045fb984f8b11f7c5924ca9d6e2d78
Ostap Yonka <ynk.otp.nl@gmail.com>
2026-07-18 18:42:34 +0300

    Updated site row in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://ynkotp228.github.io",
+  site: "https://ynkotp-webdev.github.io",
   base: "/cyberHub/",
   integrations: [sitemap()],
   vite: {
